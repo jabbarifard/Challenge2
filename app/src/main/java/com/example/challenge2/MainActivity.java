84adf8f5e7921a22958696a7fb659378eb79c0a8
Jabbarifard <jabbarifard@gmail.com>
2020-10-16 10:48:28 -0500

    new changes

--- app/src/main/java/com/example/challenge2/MainActivity.java
+++ app/src/main/java/com/example/challenge2/MainActivity.java
@@ -7,9 +7,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button insertBtn;
+    Button chooseBtn;
     int amount;
     TextView totalAmount;
     TextView moneyAmount;
@@ -22,7 +24,22 @@ public class MainActivity extends AppCompatActivity {
         totalAmount = (TextView) findViewById(R.id.totalAmount);
         moneyAmount = (EditText) findViewById(R.id.moneyAmount);
         insertBtn = (Button) findViewById(R.id.insertBtn);
+        chooseBtn = (Button) findViewById(R.id.chooseBtn);
+        chooseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Toast.makeText(getApplicationContext(),
+                        "You chose Orange Crush!", Toast.LENGTH_LONG).show();
+            }
+        });
     }
+//    public void toastMsg(String msg) {
+//        Toast toast = Toast.makeText(this, msg, Toast.LENGTH_LONG);
+//        toast.show();
+//    }
+//    public void displayToastMsg(View v) {
+//        toastMsg("You chose Orange Crush!");
+//    }
 
     public void insertOnClick(View view){
         //amount = moneyAmount.getText();//charSequence
